371e65990d5cf1756778f32a6f20b4374eee6c98
sanjankumar <sanjankumar444@gmail.com>
2017-02-15 10:31:17 +0530

    Add files via upload

---
+++ Assessment/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Yourstore</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+
+<body>
+	<nav>
+		<p>yourstore.com</p>
+	</nav>
+	
+	<div class="mainblock">
+		<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+		<div style="margin-left:22px;padding:20px;">
+		<p class="top-text">Are you looking for <b>'white dresses'</b>?</p>
+			<div class="block-1">
+				<img src="assets/images/r1.jpg" alt="" style="width:48%">
+				<img src="assets/images/r2.jpg" alt="" style="width:48%">
+				<div class="container">
+					<p class="btm-text"><b>Cotton white dresses</b><br>see more</p>  
+				</div>
+			</div>
+
+			<div class="block-2">
+			    <img src="assets/images/r3.jpg" alt="" style="width:48%">
+			    <img src="assets/images/n3.jpg" alt="" style="width:48%">
+				<div class="container">
+					<p class="btm-text"><b>Cotton dresses</b><br>see more</p> 
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</body>
+</html> 
